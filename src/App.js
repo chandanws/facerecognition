42fad6e0ebbe16aebde5e4f2b8984daf6e180167
Ivan Taghavi Espinosa <ivansezoskate@gmail.com>
2018-09-13 16:48:57 +0200

    image form and rank added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
 import React, { Component } from 'react';
+import Particles from 'react-particles-js';
 import Navigation from './components/navigation/Navigation';
 import Logo from './components/logo/Logo';
+import ImageLinkForm from './components/imageLinkForm/ImageLinkForm';
+import Rank from './components/rank/Rank';
 import 'tachyons';
 
 import './App.css';
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 30,
+      density: {
+        enable: true, 
+        value_area:800
+      }
+    }
+  }
+};
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Particles className="particles"
+              params={particlesOptions} />
         <Navigation />
         <Logo />
-        {/* <ImageLinkForm/>
-        <FaceRecognition/> */}
+        <Rank />
+        <ImageLinkForm/>
+        {/* <FaceRecognition/> */}
       </div>
     );
   }
---
+++ src/components/rank/Rank.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Rank = () => {
+    return (
+        <div>
+            <div className="white f3">
+                {'Ivan, this display the rank...'}
+            </div>
+            <div className="white f1">
+            {'#2'}
+            </div>  
+        </div>
+    )
+}
+
+export default Rank;
